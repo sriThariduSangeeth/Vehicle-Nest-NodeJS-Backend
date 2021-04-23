e6d3b3f8fb696fbad355322f16314d8ea82d8394
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-04-24 04:15:29 +0530

    delete vehicle-graphql-server

--- vehicle-graphql-server/src/vehicle/vehicle.provider.ts
+++
@@ -1,8 +0,0 @@
-import { Vehicle } from "./entity/vehicle.entity";
-
-export const VehicleProviders = [
-    {
-      provide: 'VEHICLE_REPOSITORY',
-      useValue: Vehicle,
-    },
-  ];
--- vehicle-graphql-server/src/vehicle/vehicle.resolver.ts
+++
@@ -1,6 +0,0 @@
-import { Resolver } from "@nestjs/graphql";
-import { Vehicle } from "src/models/vehicle";
-
-
-@Resolver(() => Vehicle)
-export class VehicleResolver{}
